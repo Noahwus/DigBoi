9663b51a97325ad33c2cb9d271f45ac4981e8e71
Noahwus <noahwus@gmail.com>
2017-12-23 22:52:04 -0600

    Noah's fukcing everything up

---
+++ DigBoi/Assets/Scripts/goToMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goToMainMenu : MonoBehaviour {
+
+	public void goToMainMenuScreen(){
+		SceneManager.LoadScene ("MainMenu");
+		Debug.Log ("going to MainMenu");
+	}
+}
