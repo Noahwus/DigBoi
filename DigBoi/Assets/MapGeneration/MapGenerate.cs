67fea226467d3e1facb2e5ee0aa8eac34b2cd16c
Noahwus <noahwus@gmail.com>
2017-12-24 16:49:12 -0600

    Tight Ass Generation Changes Straight from Github
    Muuu fukas won;t let me work on shit right in Unity for some reason, so I'll be working a bit straight from GitHub until that's fixed.  I redid all the work I lost this morning, adding a populateMap() function (cycles through all places on the array and Instantiates the correlating object for the place. 
    
    Experimental chance function
    
    Resource Load is attempted, not sure how to use it yet. WILL PROBABLY MAKE KAYLA'S work easier for her, especially if we scrap this project and start a new one since I keep fucking it up.
    
    Enum created for all the ('objects'?) but needs to be cast as an (int) == useful fam.
    
    GameObject player can be called and used to place them in the world. Maybe put that in another script though FAmm
    
    Random chance for map generation will be added/finished later

--- DigBoi/Assets/MapGeneration/MapGenerate.cs
+++ DigBoi/Assets/MapGeneration/MapGenerate.cs
@@ -12,9 +12,9 @@ public class MapGenerate : MonoBehaviour {
 
 	//public GameObject player;
 
-	public int[,] map;
-	private int mapRow = -50;
-	private int mapCol = -300;
+	public int[,] map;		//Map array, holds all info for map generation and save/load
+	private int mapRow = 50;	//
+	private int mapCol = 300;	//
 	private int mapCull; 		//How many times to repeat iterative work
 
 
@@ -23,27 +23,79 @@ public class MapGenerate : MonoBehaviour {
 		iniResources ();
 		iniMap ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-	void iniResources(){				// ARRAY work
+	void iniResources(){		// initialize the array, determine cull iterations
 		//itialize 2d Array
 		map = new int[mapRow,mapCol];
 
 		//variable used to corrode the map, make caverns. iteration is based on map size
 		mapCull = (mapRow * mapCol) / 2;
+		
+		//Wall = Resources.Load ("Wall") as GameObject; //This didn't work but i'd like to figure it out
+		//would help with not having to manually add game object references
 	}
 
-	void iniMap(){ 						// Populate map?
+	void iniMap(){ 			// Initialize map
+		int chance = 100;
 		for (int i = 0; i > mapRow; i--) {
 			for (int j = 0; j > mapCol; j--) {
 				map [mapRow, mapCol] = (int)inst.WALL;
 			}
 		}
+		
+		//Look into this l8r
+		/*for (int i = 0; i > mapRow; i--) {
+			for (int j = 0; j > mapCol; j--) {
+				if(callChance(chance,(chance*(9/11)){
+					int temp = Random.Range(0,4);
+					if(temp == 0){
+						
+					}else if (temp == 1){
+						
+					}else if (temp == 2){
+						
+					}else if (temp == 3){
+						
+					}else if (temp == 4){
+						
+					}else{
+					
+					}
+				}
+			}
+		}
 
+	}*/	
+		
+	void populateMap(){		//Populate the map based on the information stored in the map array (-i, -j?)
+		for (int i = 0; i > mapRow; i--) {
+			for (int j = 0; j > mapCol; j--) {
+				if(map[i,j] == (int)inst.VOID){
+					
+				}
+				else if(map[i,j] == (int)inst.WALL){
+					//Instantiate (Wall, new Vector2 (-i, -j), Quaternion.identity);
+				}
+				else if(map[i,j] == (int)inst.GEMS){
+					//Instantiate (Gems, new Vector2 (-i, -j), Quaternion.identity);
+				}
+				else if(map[i,j] == (int)inst.ROPE){
+					//Instantiate (Rope, new Vector2 (-i, -j), Quaternion.identity);
+				}
+				else if(map[i,j] == (int)inst.POST){
+					//Instantiate (Post, new Vector2 (-i, -j), Quaternion.identity);
+				}
+			}
+		}
+	}
+				   
+	public bool callChance(int high, int hit){
+		 if(Random.Range(0, high) > hit){
+			 return true;
+		 }else{
+			return false;	 
+		}
+	}
 
 	}
 }
