3b55b0cae1d111590b335e83925dc9d925ff89ed
Kayla Techmeier <k.mae.tech@gmail.com>
2017-12-23 22:15:51 -0600

    adjusted canvases and added main menu

---
+++ DigBoi/Assets/Scripts/goToGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goToGame : MonoBehaviour {
+
+
+	public void goToGameScreen(){
+		SceneManager.LoadScene ("Game");
+		Debug.Log ("going to Game");
+	}
+}
